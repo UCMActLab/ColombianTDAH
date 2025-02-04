8f4b99a2fe701a8ba14946ac3a9df4e1113592bf
efeliua <100871802+efeliua@users.noreply.github.com>
2025-02-04 12:37:23 +0100

    Anims delfín

--- Assets/Scripts/DolphinController.cs
+++ Assets/Scripts/DolphinController.cs
@@ -4,15 +4,32 @@ public class DolphinController : MonoBehaviour
 {
     protected Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    
     void Start()
     {
         animator = GetComponent<Animator>();
-        animator.Play("Jump");
+    }
+
+    void Jump()
+    {
+        animator.SetTrigger("Jump");
+    }
+    void SpecialJump()
+    {
+        animator.SetTrigger("SpecialJump");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0)) //haciendo pruebas
+        {
+            Jump();
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1)) //haciendo pruebas
+        {
+            SpecialJump();
+        }
     }
 }
