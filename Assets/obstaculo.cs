74656477cf474ff49730b5c3cb080b9341fba90b
Nahia <82345386+nahigles@users.noreply.github.com>
2025-02-26 19:58:04 +0100

    Con collider obstaculos

--- Assets/obstaculo.cs
+++ Assets/obstaculo.cs
@@ -22,6 +22,7 @@ public class Obstaculo : MonoBehaviour
     {
         if (collision.gameObject.GetComponent<DolphinController>() != null)
         {
+            Debug.Log("Chokau");
             Vector3 aux = Vector3.zero;
             aux.x = m_Vel / 10;
             aux.y = -m_Vel;
