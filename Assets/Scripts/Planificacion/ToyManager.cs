7a08811b07ab5f439c70e7d6819a4dc8c229c828
Nahia <82345386+nahigles@users.noreply.github.com>
2025-10-10 14:02:49 +0200

    Figuras sin desbloqueo

---
+++ Assets/Scripts/Planificacion/ToyManager.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToyManager : MonoBehaviour
+{
+    List<GameObject> _toys;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _toys = new List<GameObject>();
+
+        // Consigo los hijos para tener las figuras
+        foreach (Transform child in transform)
+        {
+            _toys.Add(child.gameObject);
+        }
+
+        // Quito filtro en negro a los conseguidos
+        //int collec = 1;
+        //List<bool[]> info = SceneLoader.Instance.GetCollectablesInfo();
+        //for (int i = 0; i < info.Count; i++)
+        //{
+        //    if (info[i][collec])
+        //    {
+        //        UnlockToy(i);
+        //    }
+        //}
+    }
+
+    void UnlockToy(int index)
+    {
+
+    }
+}
